b268b0d570f20fe5e3e44dcb86e44329afd760e9
Milena Nunes <38266476+lenna2002@users.noreply.github.com>
2020-03-02 21:02:38 -0300

    Update

--- TIMER.vhd
+++ TIMER.vhd
@@ -1,143 +1,113 @@
-	library IEEE;
-	use IEEE.std_logic_1164.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
 
-	entity TIMER is
-		port(
-					min_dec, min_un, sec_dec, sec_un	:		out	std_logic_vector(6 downto 0);
-					clk	:		 in	std_logic);			--ainda tem que adicionar os botões
-	end TIMER;
-		
-	architecture hardware of TIMER is
+entity fpga is
+	port (
+			bar_un_sec		: out std_logic_vector(6 downto 0);
+			bar_dec_sec 		: out std_logic_vector(6 downto 0);
+			bar_un_min 		: out std_logic_vector(6 downto 0);
+			bar_dec_min 		: out std_logic_vector(6 downto 0);
+			clk			: in 	std_logic;
+			reset			: in	std_logic);
+			set			: in	std_logic);
+end fpga;
+
+architecture hardwere of fpga is
+begin
+	process(clk, set, reset)
+		variable un_sec 	: integer := 9;
+		variable dec_sec 	: integer := 5;
+		variable un_min 	: integer := 9;
+		variable dec_min 	: integer := 5;
 	begin
-		process(clk)
 		
-			-- Vai ter modificar abaixo para receber o valor da placa
-				variable display4 : integer;					--display das unidades dos segundos
-				variable display3 : integer;					--display dos decimais dos segundos
-				variable display2 : integer;					--display das unidades dos minutos
-				variable display1 : integer;					--display dos decimais dos minutos
-				
-		begin			
+		if(clk = '1') then						--esses são os if para poder ir reduzindo a contagem
+			un_sec := un_sec - 1;
+			if(un_sec = 0) then
+				if(dec_sec > 0) then
+					dec_sec := dec_sec - 1;
+					un_sec := 9;
+				else 
+					if(un_min > 0) then
+						un_min := un_min - 1;
+						dec_sec := 5;
+						un_sec := 9;
+					else
+						if(dec_min > 0) then
+							dec_min := dec_min - 1;
+							un_min := 9;
+							dec_sec := 5;
+							un_sec := 9;
+						else 
+							dec_min := 0;
+							un_min := 0;
+							dec_sec := 0;
+							un_sec := 0;
+						end if
+					end if
+				end if
+			end if
+		end if;
 		
+		case un_sec is
 			
-			case display4 is
-			
-				 when 0 => sec_un <= "1111110";
-				 when 1 => sec_un <= "0110000";
-				 when 2 => sec_un <= "1101101";
-				 when 3 => sec_un <= "1111001";
-				 when 4 => sec_un <= "0110011";
-				 when 5 => sec_un <= "1011011";
-				 when 6 => sec_un <= "1011111";
-				 when 7 => sec_un <= "1110000";
-				 when 8 => sec_un <= "1111111";
-				 when 9 => sec_un <= "1111011";
-				 when others => sec_un <= "1111110";
+			when 0 => bar_un_sec <= "1111110";
+			when 1 => bar_un_sec <= "0110000";
+			when 2 => bar_un_sec <= "1101101";
+			when 3 => bar_un_sec <= "1111001";
+			when 4 => bar_un_sec <= "0110011";
+			when 5 => bar_un_sec <= "1011011";
+			when 6 => bar_un_sec <= "1011111";
+			when 7 => bar_un_sec <= "1110000";
+			when 8 => bar_un_sec <= "1111111";
+			when 9 => bar_un_sec <= "1111011";
+			when others => bar_un_sec <= "1111110";
 				 
-			end case;
-			
-			case display3 is
-			
-				 when 0 => sec_dec <= "1111110";
-				 when 1 => sec_dec <= "0110000";
-				 when 2 => sec_dec <= "1101101";
-				 when 3 => sec_dec <= "1111001";
-				 when 4 => sec_dec <= "0110011";
-				 when 5 => sec_dec <= "1011011";
-				 when others => sec_dec <= "1111110";
-				 
-			end case;
-			
-			case display2 is
-			
-				 when 0 => min_un <= "1111110";
-				 when 1 => min_un <= "0110000";
-				 when 2 => min_un <= "1101101";
-				 when 3 => min_un <= "1111001";
-				 when 4 => min_un <= "0110011";
-				 when 5 => min_un <= "1011011";
-				 when 6 => min_un <= "1011111";
-				 when 7 => min_un <= "1110000";
-				 when 8 => min_un <= "1111111";
-				 when 9 => min_un <= "1111011";
-				 when others => min_un <= "1111110";
-				 
-			end case;
-			
-			case display1 is
-			
-				 when 0 => min_dec <= "1111110";
-				 when 1 => min_dec <= "0110000";
-				 when 2 => min_dec <= "1101101";
-				 when 3 => min_dec <= "1111001";
-				 when 4 => min_dec <= "0110011";
-				 when 5 => min_dec <= "1011011";
-				 when others => min_dec <= "1111110";
-				 
-			end case;
-			
-			--if (min_dec > 6) then					--esses if são para reduzir os valores dos minutos e dos segundos para o seu valor máximo, caso tenha sido colocado algum valor superior
-			--min_dec := 5;
-			--end if;
-			
-			--if (sec_dec > 6) then
-			--sec_dec := 5;
-			--end if;
-			
-			--if (sec_un > 9) then
-			--sec_un := 9;
-			--end if;
-			
-			--if (min_un > 9) then
-			--min_un := 9;
-			--end if;
-			
-			while (min_dec < 6) loop				--quando chegar tudo em zero, eu vou colocar um valor maior para poder desligar todos os leds
-				
-				if(clk = '1') then						--esses são os if para poder ir reduzindo a contagem
-
-					sec_un := sec_un - 1;
-					
-					if (sec_un = 0) then						--aqui é para poder ir reduzindo os displays seguintes
-					
-						if (sec_dec = 0) then
-						
-							if (min_un > 0) then				--esse aqui é para testar se tem algum valor no display das unidades dos minutos para reduzir quando o das dezenas dos segundos chegar a zero
-							
-								min_un := min_un -1;
-								sec_dec := 5;
-								sec_un := 9;
-								
-							else if (min_un = 0 and min_dec > 0) then				--esse aqui é para ir para o display de decimais dos minutos quando o das unidades acabar
-								min_dec := min_dec -1;
-								min_un := 9;
-								sec_dec := 5;
-								sec_un := 9;
-							end if;
-						
-						end if;
-						
-						if (sec_dec > 0) then
-						
-							sec_dec := sec_dec -1;
-							sec_un := 9;
-							
-						end if;
-					end if;
-					
-					if (sec_dec = 0 and min_un = 0 and sec_dec = 0 and sec_un = 0) then				--esses if são para poder parar o loop quando tudo chegar a zero, e então colocar valores que não são suportados para que os displays sejam desligados.
-						
-						sec_un := 10;
-						sec_dec := 10;
-						min_un := 10;
-						min_dec := 10;
-						
-					end if;
-				
-				end if;
-			
-			end loop;
-			
-		end process;
+		end case;
 		
-	end hardware;
+		case dec_sec is
+			
+			when 0 => bar_dec_sec <= "1111110";
+			when 1 => bar_dec_sec <= "0110000";
+			when 2 => bar_dec_sec <= "1101101";
+			when 3 => bar_dec_sec <= "1111001";
+			when 4 => bar_dec_sec <= "0110011";
+			when 5 => bar_dec_sec <= "1011011";
+			when others => bar_dec_sec <= "1111110";
+				 
+		end case;
+			
+		case un_min is
+			
+			when 0 => bar_un_min <= "1111110";
+			when 1 => bar_un_min <= "0110000";
+			when 2 => bar_un_min <= "1101101";
+			when 3 => bar_un_min <= "1111001";
+			when 4 => bar_un_min <= "0110011";
+			when 5 => bar_un_min <= "1011011";
+			when 6 => bar_un_min <= "1011111";
+			when 7 => bar_un_min <= "1110000";
+			when 8 => bar_un_min <= "1111111";
+			when 9 => bar_un_min <= "1111011";
+			when others => bar_un_min <= "1111110";
+				 
+		end case;
+			
+		case dec_min is
+			
+			when 0 => bar_dec_min <= "1111110";
+			when 1 => bar_dec_min <= "0110000";
+			when 2 => bar_dec_min <= "1101101";
+			when 3 => bar_dec_min <= "1111001";
+			when 4 => bar_dec_min <= "0110011";
+			when 5 => bar_dec_min <= "1011011";
+			when others => bar_dec_min <= "1111110";
+				 
+		end case;
+		
+		if (dec_sec = 0 and un_min = 0 and dec_min = 0 and un_sec = 0) then				--esses if são para poder parar o loop quando tudo chegar a zero, e então colocar valores que não são suportados para que os displays sejam desligados.
+			exit;
+		end if;
+	
+	end process;
+end hardwere;	
